ea190e7feff0cc07246699619e354386633d04d0
Waqas Younis <86519858+waqas-334@users.noreply.github.com>
2024-08-14 05:28:01 -0700

    12 Setting up Open Food API - Completed

---
+++ tracker/tracker_data/src/main/java/com/waqas/tracker_data/remote/OpenFoodApi.kt
@@ -0,0 +1,18 @@
+package com.waqas.tracker_data.remote
+
+import com.waqas.tracker_data.remote.dto.SearchDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OpenFoodApi {
+
+    @GET("cgi/search.pl?search_simple=1&json=1&action=process&fields=product_name,nutriments,image_front_thumb_url")
+    suspend fun searchFood(
+        @Query("search_term") query: String,
+        @Query("page") page: Int,
+        @Query("page_size") pageSize: Int
+    ): SearchDto
+    companion object {
+        const val BASE_URL = "https://us.openfoodfacts.org/"
+    }
+}
---
+++ tracker/tracker_data/src/main/java/com/waqas/tracker_data/remote/dto/SearchDto.kt
@@ -0,0 +1,6 @@
+package com.waqas.tracker_data.remote.dto
+
+
+data class SearchDto(
+    val products: List<Product>,
+)
