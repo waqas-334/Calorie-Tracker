1dc4c2e7025cf12d346634a41bc678a2fc38c88d
Waqas Younis <86519858+waqas-334@users.noreply.github.com>
2024-08-20 06:51:00 -0700

    21 SearchViewModel - completed

--- tracker/tracker_presentation/src/main/java/com/waqas/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/waqas/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,5 +1,6 @@
 package com.waqas.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -8,13 +9,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.waqas.core.util.UiEvent
 import com.waqas.core_ui.LocalSpacing
+import com.waqas.tracker_presentation.tracker_overview.components.AddButton
 import com.waqas.tracker_presentation.tracker_overview.components.DaySelector
 import com.waqas.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.waqas.tracker_presentation.tracker_overview.components.NutrientsHeader
+import com.waqas.tracker_presentation.tracker_overview.components.TrackedFoodItem
 
 @Composable
 fun TrackerOverviewScreen(
@@ -55,7 +60,39 @@ fun TrackerOverviewScreen(
                 onToggleClick = {
                     viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
                 },
-                content = {},
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+                        state.trackedFoods.forEach {
+                            TrackedFoodItem(
+                                trackedFood = it,
+                                onDeleteClick = {
+                                    viewModel.onEvent(
+                                        TrackerOverviewEvent.OnDeleteTrackedFoodClick(it)
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        }
+                        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        AddButton(
+                            text = stringResource(
+                                id = com.waqas.core.R.string.add_meal,
+                                meal.name.asString(context)
+                            ),
+                            onClick = {
+                                viewModel.onEvent(
+                                    TrackerOverviewEvent.OnAddFoodClick(meal)
+                                )
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                    }
+                },
                 modifier = Modifier.fillMaxWidth()
             )
         }
---
+++ tracker/tracker_presentation/src/main/java/com/waqas/tracker_presentation/tracker_overview/components/TrackerFoodItem.kt
@@ -0,0 +1,153 @@
+package com.waqas.tracker_presentation.tracker_overview.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.waqas.core.compose.R
+import com.waqas.core_ui.LocalSpacing
+import com.waqas.tracker_domain.model.TrackedFood
+import com.waqas.tracker_presentation.components.NutrientsInfo
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun TrackedFoodItem(
+    trackedFood: TrackedFood,
+    onDeleteClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    val spacing = LocalSpacing.current
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(5.dp))
+            .padding(spacing.spaceExtraSmall)
+            .shadow(elevation = 1.dp, shape = RoundedCornerShape(5.dp))
+            .background(MaterialTheme.colors.surface)
+            .padding(end = spacing.spaceMedium)
+            .height(100.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Image(
+            painter = rememberImagePainter(data = trackedFood.imageUrl,
+                builder = {
+                    crossfade(true)
+                    error(com.waqas.core.R.drawable.ic_burger)
+                    fallback(com.waqas.core.R.drawable.ic_burger)
+                }),
+            contentDescription = trackedFood.name,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxHeight()
+                .aspectRatio(1f)
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 5.dp,
+                        bottomStart = 5.dp
+                    )
+                )
+
+        )
+
+        Spacer(modifier = Modifier.width(spacing.spaceMedium))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = trackedFood.name ?: "",
+                style = MaterialTheme.typography.body1,
+                maxLines = 2,
+                overflow = Ellipsis
+            )
+
+            Spacer(modifier = Modifier.height(spacing.spaceExtraSmall))
+            Text(
+                text = stringResource(
+                    id = com.waqas.core.R.string.nutrient_info,
+                    trackedFood.amount,
+                    trackedFood.calories
+                ),
+                style = MaterialTheme.typography.body2
+            )
+            Spacer(modifier = Modifier.width(spacing.spaceMedium))
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = stringResource(id = com.waqas.core.R.string.delete),
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .clickable { onDeleteClick() }
+                )
+
+                Spacer(modifier = Modifier.height(spacing.spaceExtraSmall))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    NutrientsInfo(
+                        name = stringResource(id = com.waqas.core.R.string.carbs),
+                        amount = trackedFood.carbs,
+                        unit = stringResource(id = com.waqas.core.R.string.grams),
+                        amountTextSize = 16.sp,
+                        unitTextSize = 12.sp,
+                        nameTextStyle = MaterialTheme.typography.body2
+                    )
+                    Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                    NutrientsInfo(
+                        name = stringResource(id = com.waqas.core.R.string.protein),
+                        amount = trackedFood.protein,
+                        unit = stringResource(id = com.waqas.core.R.string.grams),
+                        amountTextSize = 16.sp,
+                        unitTextSize = 12.sp,
+                        nameTextStyle = MaterialTheme.typography.body2
+                    )
+                    Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                    NutrientsInfo(
+                        name = stringResource(id = com.waqas.core.R.string.fat),
+                        amount = trackedFood.fat,
+                        unit = stringResource(id = com.waqas.core.R.string.grams),
+                        amountTextSize = 16.sp,
+                        unitTextSize = 12.sp,
+                        nameTextStyle = MaterialTheme.typography.body2
+                    )
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+
+}
