187d8042d3268490774014af25f83bc757fa99b8
waqas-334 <waqasyounis334@gmail.com>
2024-08-25 19:08:45 -0230

    Fixed same API responses regardless of the query

--- tracker/tracker_data/src/main/java/com/waqas/tracker_data/remote/OpenFoodApi.kt
+++ tracker/tracker_data/src/main/java/com/waqas/tracker_data/remote/OpenFoodApi.kt
@@ -8,10 +8,11 @@ interface OpenFoodApi {
 
     @GET("cgi/search.pl?search_simple=1&json=1&action=process&fields=product_name,nutriments,image_front_thumb_url")
     suspend fun searchFood(
-        @Query("search_term") query: String,
+        @Query("search_terms") query: String,
         @Query("page") page: Int,
         @Query("page_size") pageSize: Int
     ): SearchDto
+
     companion object {
         const val BASE_URL = "https://us.openfoodfacts.org/"
     }
